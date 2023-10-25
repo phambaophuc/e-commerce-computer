602139844c1c58c68bffba593bcdb5eed0af5107
Pham Bao Phuc <123phuc27602@gmail.com>
2023-10-25 16:08:11 +0700

    delete object aws s3

--- src/main/java/DoAnJava/LinhKienDienTu/utils/S3Util.java
+++ src/main/java/DoAnJava/LinhKienDienTu/utils/S3Util.java
@@ -7,8 +7,8 @@ import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,12 +19,9 @@ public class S3Util {
     private static final String BUCKET = "phuc-public-image";
 
     public static void uploadFile(String fileName, InputStream inputStream)
-            throws S3Exception, AwsServiceException, SdkClientException, IOException {
+            throws AwsServiceException, SdkClientException, IOException {
 
-        S3Client client = S3Client.builder()
-                .region(Region.AP_SOUTHEAST_2)
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY)))
-                .build();
+        S3Client client = createS3Client();
 
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(BUCKET)
@@ -34,5 +31,25 @@ public class S3Util {
                 .build();
 
         client.putObject(request, RequestBody.fromInputStream(inputStream, inputStream.available()));
+    }
+
+    public static void deleteFile(String fileName) {
+        S3Client client = createS3Client();
+
+        DeleteObjectRequest request = DeleteObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(fileName)
+                .build();
+
+        client.deleteObject(request);
+    }
+
+    private static S3Client createS3Client() {
+        S3Client client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY)))
+                .build();
+
+        return client;
     }
 }
